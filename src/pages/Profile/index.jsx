528c11917ad88bde9facd478c70282391e5c548b
max <max.yuri.13@hotmail.com>
2023-10-18 19:02:26 -0300

    User Profile Finished

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,59 @@
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from 'react-icons/fi';
+import { Input } from '../../components/Input'
+import { Button } from '../../components/Button'
+import { Container, Form, Avatar } from "./styles";
+
+export function Profile() {
+  return (
+    <Container>
+      <header>
+        <a href="/">
+          <FiArrowLeft/>
+        </a>
+      </header>
+      <Form>
+        <Avatar>
+          <img 
+            src="https://github.com/maxyuri13.png" 
+            alt="User photo"
+          />
+          <label htmlFor="avatar">
+            <FiCamera/>
+
+            <input 
+              id="avatar"
+              type="file" 
+            />
+          </label>
+            
+        </Avatar>
+        <Input
+          placeholder="Name"
+          type="text"
+          icon={FiUser}
+        />
+
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input
+          placeholder="Current password"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="New password"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Save"/>
+      </Form>
+
+    </Container>
+  )
+}
