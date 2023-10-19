0bf9a85894937f66cf50447d3a334fe471589c19
max <max.yuri.13@hotmail.com>
2023-10-19 11:05:40 -0300

    finish create note

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,37 @@
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input'
+import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
+import { Button } from '../../components/Button'
+
+import { Container, Form } from './styles';
+
+export function New() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <Form>
+          <header>
+            <h1>Create note</h1>
+            <a href="/">Back</a>
+          </header>
+          <Input placeholder="Title" />
+          <Textarea placeholder="Observations"/>
+          <Section title="Useful links">
+            <NoteItem value="https://rocketseat.com.br"/>
+            <NoteItem isNew placeholder="New link" />
+          </Section>
+          <Section title="Markers">
+            <div className="tags">
+              <NoteItem value="react"/>
+              <NoteItem isNew placeholder="New tag" />
+            </div>
+          </Section>
+          <Button title="Save"/>
+        </Form>
+      </main>
+    </Container>
+  );
+}
